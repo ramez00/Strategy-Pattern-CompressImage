d10e976abbc5fd3c63f5a136db88590dcb0017a8
ramez.abdelhamid@health-insights.com <ramez.abdelhamid@health-insights.com>
2025-05-11 22:15:06 +0300

    Add project files.

---
+++ Strategy-Pattern-CompressImage/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Welcome To our App to convert Image");
+Console.WriteLine("Plz Select Image Type ");
+Console.WriteLine("[1].JPEG ");
+Console.WriteLine("[2].PNG ");
+Console.WriteLine("[3].GIF ");
+
+int choise;
+if (int.TryParse(Console.ReadLine(), out choise))
+{
+    Loading(); 
+    switch (choise)
+    {
+        case 1:
+            Console.WriteLine("You have selected JPEG");
+            break;
+        case 2:
+            Console.WriteLine("You have selected PNG");
+            break;
+        case 3:
+            Console.WriteLine("You have selected GIF");
+            break;
+        default:
+            Console.WriteLine("Invalid selection");
+            break;
+    }
+}
+
+void Loading()
+{
+    Console.Write("Loading...");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write(".");
+        Thread.Sleep(500);
+    }
+    Console.WriteLine();
+}
